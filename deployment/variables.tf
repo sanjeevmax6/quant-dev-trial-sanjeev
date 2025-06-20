25e3716ffdfc8773529a63cb8a6b7a29dc72bfb3
Sanjeev Vijayakumar <sanvegas2001@gmail.com>
2025-06-20 14:01:55 -0400

    workaround to scp dataset to EC2

--- deployment/variables.tf
+++ deployment/variables.tf
@@ -2,3 +2,8 @@ variable "public_key_path" {
   description = "Path to your SSH public key"
   default     = "~/.ssh/id_rsa.pub"
 }
+
+variable "csv_file_path" {
+  description = "Path to l1_day.csv"
+  default     = "./l1_day.csv"
+}
