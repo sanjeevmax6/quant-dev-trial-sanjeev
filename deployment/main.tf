8b08e9a55efd0260f37c991fc43dc08999de4fcf
Sanjeev Vijayakumar <sanvegas2001@gmail.com>
2025-06-20 13:44:04 -0400

    added functionality to scp the dataset into EC2

--- deployment/main.tf
+++ deployment/main.tf
@@ -33,13 +33,19 @@ resource "aws_security_group" "quant_sg" {
   }
 }
 
+data "local_file" "csv_data" {
+  filename = var.csv_file_path
+}
+
 resource "aws_instance" "quant_instance" {
   ami                    = "ami-053b0d53c279acc90" # Ubuntu 20.04 (us-east-1)
   instance_type          = "t3.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.quant_sg.id]
-
-  user_data = file("${path.module}/bootstrap.sh")
+  
+  user_data = templatefile("${path.module}/scripts/bootstrap.sh", {
+    csv_placeholder = data.local_file.csv_data.content
+  })
 
   tags = {
     Name = "QuantSORInstance"
--- deployment/variables.tf
+++ deployment/variables.tf
@@ -2,3 +2,8 @@ variable "public_key_path" {
   description = "Path to your SSH public key"
   default     = "~/.ssh/id_rsa.pub"
 }
+
+variable "csv_file_path" {
+  description = "Path to the l1_day.csv file"
+  default     = "./l1_day.csv"
+}
