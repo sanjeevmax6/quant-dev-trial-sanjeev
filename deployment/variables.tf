6c3f912a5e779e322d98be7c3d7df719d1d84173
Sanjeev Vijayakumar <sanvegas2001@gmail.com>
2025-06-20 14:05:37 -0400

    merged

--- deployment/variables.tf
+++ deployment/variables.tf
@@ -7,3 +7,8 @@ variable "csv_file_path" {
   description = "Path to l1_day.csv"
   default     = "./l1_day.csv"
 }
+
+variable "private_key_path" {
+  description = "Path to your private key used for SSH"
+  default     = "~/.ssh/id_rsa"
+}
